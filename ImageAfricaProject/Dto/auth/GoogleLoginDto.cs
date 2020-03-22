aaf8fda1d61abf133d36b2b14ea7e6e81e4d7a5d
IYERITUFU precious <iyeritufu@gmail.com>
2020-03-22 01:08:05 +0100

    change googleLoginDto

--- ImageAfricaProject/Dto/auth/GoogleLoginDto.cs
+++ ImageAfricaProject/Dto/auth/GoogleLoginDto.cs
@@ -7,8 +7,8 @@ namespace ImageAfricaProject.Dto.auth
 {
     public class GoogleLoginDto
     {
-        public string Provider { get; set; } = "Google";
+        //public string Provider { get; set; } = "Google";
         public string IdToken {get;set;}
-        public string ReturnUrl { get; set; } = null;
+        //public string ReturnUrl { get; set; } = null;
     }
 }
