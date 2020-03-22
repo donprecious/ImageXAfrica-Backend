95af295b02b42fea5021212c90cf5ad2b7bf4d8b
IYERITUFU precious <iyeritufu@gmail.com>
2020-03-22 08:38:18 +0100

    added sentry error logging

--- ImageAfricaProject/Program.cs
+++ ImageAfricaProject/Program.cs
@@ -21,6 +21,7 @@ namespace ImageAfricaProject
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseSentry("https://16c78b8d3c19451cbc8e1e9aeb86bfcf@sentry.io/5170489");
                 });
     }
 }
