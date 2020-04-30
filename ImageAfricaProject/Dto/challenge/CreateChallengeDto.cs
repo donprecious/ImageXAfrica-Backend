52857cf41695566ae446b7a1d25ce1d480be2af8
guzzit <akuomajoshua@gmail.com>
2020-04-30 13:27:18 +0100

    Added data annotations to CreateChallengeDto

--- ImageAfricaProject/Dto/challenge/CreateChallengeDto.cs
+++ ImageAfricaProject/Dto/challenge/CreateChallengeDto.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace ImageAfricaProject.Dto.challenge
 {
@@ -11,12 +12,16 @@ namespace ImageAfricaProject.Dto.challenge
         {
             CreationTime = DateTime.UtcNow;
         }
+        [Required(ErrorMessage = "Title Required")]
         public string Name { get; set; }
         public string Description { get; set; }
         public string TipsAndConditions { get; set; }
+        [Required(ErrorMessage = "Start Date Required")]
         public DateTime StartDate { get; set; }
+        [Required(ErrorMessage = "End Date Required")]
         public DateTime EndDate { get; set; }
         public string Prize { get; set; }
+        [Required(ErrorMessage = "Organizer's Name Required")]
         public string OrganizerName { get; set; }
         public string OrganizerUrl { get; set; }
         public DateTime CreationTime { get; set; }
