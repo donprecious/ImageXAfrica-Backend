83ad269d214c914b5c8489bfa6d2614869d32e07
IYERITUFU precious <iyeritufu@gmail.com>
2020-05-13 04:36:41 +0100

    added exception middleware for remote errors

--- ImageAfricaProject/Startup.cs
+++ ImageAfricaProject/Startup.cs
@@ -167,7 +167,7 @@ namespace ImageAfricaProject
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseMiddleware<ExceptionHandler>();
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
